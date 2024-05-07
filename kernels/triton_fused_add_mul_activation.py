f6edc363b21fb65c3c6c023daa9f048f5c2edc54
Kapil Sharma <kapilsharma.frm@gmail.com>
2024-05-07 06:15:20 -0400

    Generalized fused kernel

--- kernels/triton_fused_add_mul_activation.py
+++ kernels/triton_fused_add_mul_activation.py
@@ -4,8 +4,12 @@ import triton.language as tl
 
 
 @triton.jit
-def fused_add_mul_sigmoid_kernel(x_ptr, bias_ptr, in_ptr, num_weights, xnumel, multiplier,
-                               BLOCK_SIZE: tl.constexpr):
+def fused_add_mul_activation_kernel(x_ptr, bias_ptr, in_ptr,
+                                    num_weights: tl.constexpr,
+                                    xnumel: tl.constexpr,
+                                    multiplier: tl.constexpr,
+                                    activation: tl.constexpr,
+                                    BLOCK_SIZE: tl.constexpr):
     xoffset = tl.program_id(0) * BLOCK_SIZE
     index = xoffset + tl.arange(0, BLOCK_SIZE)[:]
     mask = index < xnumel
@@ -13,25 +17,36 @@ def fused_add_mul_sigmoid_kernel(x_ptr, bias_ptr, in_ptr, num_weights, xnumel, m
     tmp0 = tl.load(x_ptr + index, mask)
     tmp1 = tl.load(bias_ptr + bias_index, mask, eviction_policy='evict_last')
     tmp3 = tl.load(in_ptr + index, mask)
-    sigmoid_input = multiplier * tmp3 + tmp0 + tmp1
-    ma_result = tl.sigmoid(sigmoid_input)
-    # option 2 - calculate sigmoid using exp
-    # ma_result = 1.0 / (1.0 + tl.exp(-sigmoid_input))
-    # option 3: fast sigmoid - inaccurate but faster
-    # ma_result = 1.0 / (1.0 + tl.abs(sigmoid_input))
+    activ_input = multiplier * tmp3 + tmp0 + tmp1
+    if activation == "sigmoid":
+        ma_result = tl.sigmoid(activ_input)
+        # option 2 - calculate sigmoid using exp
+        # ma_result = 1.0 / (1.0 + tl.exp(-sigmoid_input))
+        # option 3: fast sigmoid - inaccurate but faster
+        # ma_result = 1.0 / (1.0 + tl.abs(sigmoid_input))
+    elif activation == "relu":
+        ma_result = tl.maximum(0, activ_input)
+
     tl.store(x_ptr + index, ma_result, mask)
 
-def fused_add_mul_sigmoid_torch(in_out_tensor: torch.Tensor, bias: torch.Tensor, in_tensor: torch.Tensor) -> torch.Tensor:
+
+def fused_add_mul_activation_torch(in_out_tensor: torch.Tensor, bias: torch.Tensor,
+                                   in_tensor: torch.Tensor) -> torch.Tensor:
     # print("calling fused_add_mul_relu_torch")
     grid = lambda meta: (triton.cdiv(in_out_tensor.numel(), meta['BLOCK_SIZE']),)
     BLOCK_SIZE = min(2048, in_out_tensor.numel())
-    fused_add_mul_sigmoid_kernel[grid](in_out_tensor, bias, in_tensor,
-                                       bias.numel(), in_out_tensor.numel(), multiplier=0.5,
-                                       BLOCK_SIZE=BLOCK_SIZE)
+    fused_add_mul_activation_kernel[grid](in_out_tensor, bias, in_tensor,
+                                          bias.numel(),
+                                          in_out_tensor.numel(),
+                                          multiplier=0.5,
+                                          activation="sigmoid",
+                                          BLOCK_SIZE=BLOCK_SIZE)
     return in_out_tensor
 
-def add_mul_sigmoid_torch(in_out_tensor: torch.Tensor, bias: torch.Tensor, in_tensor: torch.Tensor) -> torch.Tensor:
+
+def add_mul_activation_torch(in_out_tensor: torch.Tensor, bias: torch.Tensor, in_tensor: torch.Tensor) -> torch.Tensor:
     return torch.sigmoid(in_out_tensor + 0.5 * in_tensor + bias)
+
 
 def get_inputs(batch_size: int = 8, weight_size: int = 8, add_manual_size: bool = False):
     if add_manual_size:
@@ -43,7 +58,8 @@ def get_inputs(batch_size: int = 8, weight_size: int = 8, add_manual_size: bool 
     return in_out_tensor, in_tensor, bias
 
 
-add_mul_sigmoid_torch_scripted = torch.jit.script(add_mul_sigmoid_torch, example_inputs=[get_inputs()])
+add_mul_activation_torch_scripted = torch.jit.script(add_mul_activation_torch, example_inputs=[get_inputs()])
+
 
 @triton.testing.perf_report(
     triton.testing.Benchmark(
@@ -63,15 +79,17 @@ def benchmark(batch_size, weight_size, provider):
     in_out_tensor, in_tensor, bias = get_inputs(batch_size=batch_size, weight_size=weight_size, add_manual_size=True)
     quantiles = [0.5, 0.2, 0.8]
     if provider == "triton":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: fused_add_mul_sigmoid_torch(in_out_tensor, bias, in_tensor),
-                                                     quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(
+            lambda: fused_add_mul_activation_torch(in_out_tensor, bias, in_tensor),
+            quantiles=quantiles)
     if provider == "torch":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add_mul_sigmoid_torch(in_out_tensor, bias, in_tensor),
+        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add_mul_activation_torch(in_out_tensor, bias, in_tensor),
                                                      quantiles=quantiles)
 
     if provider == "torch_scripted":
-        ms, min_ms, max_ms = triton.testing.do_bench(lambda: add_mul_sigmoid_torch_scripted(in_out_tensor, bias, in_tensor),
-                                                     quantiles=quantiles)
+        ms, min_ms, max_ms = triton.testing.do_bench(
+            lambda: add_mul_activation_torch_scripted(in_out_tensor, bias, in_tensor),
+            quantiles=quantiles)
     gbps = lambda ms: 12 * (batch_size * weight_size) / ms * 1e-6
     return gbps(ms), gbps(max_ms), gbps(min_ms)
 
